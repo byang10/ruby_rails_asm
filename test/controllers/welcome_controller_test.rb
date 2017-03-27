17e7ca6229a54a41a7e23555813af91bf365aace
Erik Jacobson <erik.jacobson@mnsu.edu>
2017-03-27 00:04:32 +0000

    Fixed naming

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class WelcomeControllerControllerTest < ActionController::TestCase
+class WelcomeControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
