7e6568bf20006b5873222c22fcf8c819519e6988
Minhaz Uddin <minhaz.uddin@mnsu.edu>
2017-03-28 17:16:32 +0000

    Added auth

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+    def new
+    end
+    
+    def create
+       user = User.authenticate(params[:email], params[:password])
+       if user
+           session[:user_id] = user.id
+           redirect_to entries_path
+       else
+           flash.now.alert = "Invalid email or password"
+           render "/"
+       end
+    end
+end
+
+
+def destroy
+  session[:user_id] = nil
+  redirect_to root_url, :notice => "Logged out!"
+end
