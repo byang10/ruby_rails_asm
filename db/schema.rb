9997096312a89fc67ca3b9bd90d134b8737e95d2
Erik Jacobson <erik.jacobson@mnsu.edu>
2017-04-09 23:38:03 +0000

    Updated migration and added new views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170328002355) do
+ActiveRecord::Schema.define(version: 20170409230853) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",        null: false
+    t.string   "description", null: false
+    t.boolean  "positive"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "entries", force: :cascade do |t|
     t.datetime "started_at", null: false
